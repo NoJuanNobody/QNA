c916519ce44ef03018cd71ca8e98be9e2b2ab58c
alejandro londono <alejandrolondono@alejandros-MacBook-Pro.local>
2018-03-19 21:04:50 -0400

    continuing changes on VBA buttons

--- src/helpers/mso.js
+++ src/helpers/mso.js
@@ -1,5 +1,5 @@
 // Example file src/helpers/bold.js
 module.exports = function(options) {
-    var mso ='<v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://www.EXAMPLE.com/" style="height:45px;v-text-anchor:middle;width:150px;" arcsize="5%" strokecolor="#668cb3" fillcolor="#668cb3"><w:anchorlock/><center style="color:#ffffff;font-family:Amplitude, Arial, sans-serif;font-size:14px;font-weight:bold; text-transform: uppercase">'+options.fn(this)+'</center> </v:roundrect>';
+    var mso ='<v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://www.EXAMPLE.com/" style="height:45px;v-text-anchor:middle;width:150px;" arcsize="5%" strokecolor="#668cb3" fillcolor="#668cb3"><w:anchorlock/><center style="color:#ffffff;font-family:Arial, sans-serif;font-size:14px;font-weight:light; text-transform: uppercase">'+options.fn(this)+'</center> </v:roundrect>';
     return mso;
 }

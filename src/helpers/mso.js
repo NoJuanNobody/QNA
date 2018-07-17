8f79ab98dea572e4120f2a5d8589576451eb5840
gohandrogo <alondono11@gmail.com>
2018-07-17 13:59:59 -0400

    preparing ask question for system integration

--- src/helpers/mso.js
+++ src/helpers/mso.js
@@ -1,5 +1,5 @@
 // Example file src/helpers/bold.js
 module.exports = function(options) {
-    var mso ='<v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://www.google.com/" style="height:45px;v-text-anchor:middle;width:250px;" arcsize="5%" strokecolor="#668cb3" fillcolor="#668cb3"><w:anchorlock/><center style="color:#ffffff;font-family:sans-serif;font-size:14px;text-transform: uppercase">'+options.fn(this)+'</center> </v:roundrect>';
+    var mso ='<v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="[base_url]" style="height:45px;v-text-anchor:middle;width:250px;" arcsize="5%" strokecolor="#668cb3" fillcolor="#668cb3"><w:anchorlock/><center style="color:#ffffff;font-family:sans-serif;font-size:14px;text-transform: uppercase">'+options.fn(this)+'</center> </v:roundrect>';
     return mso;
 };
